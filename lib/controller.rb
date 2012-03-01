0f6b23b11f64b815b6bc5f2e8e52e4a2828f385e
Raphael Robatsch <raphaelr@f-m.fm>
2012-03-01 21:31:03 +0100

    messed up array lengths

--- lib/controller.rb
+++ lib/controller.rb
@@ -16,19 +16,18 @@ module Convolution
 			@mode = :convolution
 			
 			@input = (0...@samples).collect { |x| @peak*Math.sin(ExMath.period_f * x) }
-			@response = Array.new(samples, 0)
+			@response = Array.new(response_impulses, 0)
 			@output = Array.new(samples+response_impulses, 0)
 		end
 		
 		def update
+			resp = @response
+			resp = resp.reverse if @mode == :correlation
+			
 			@output.fill(0)
 			(0...@samples).each do |i|
 				(0...@response_impulses).each do |r|
-					response_index = case @mode
-						when :convolution then r
-						when :correlation then -r
-					end
-					@output[i + r] += @input[i] * @response[response_index]
+					@output[i + r] += @input[i] * resp[r]
 				end
 			end
 		end
